822588b440997588e78eada5aa5450ef90754264
ryoichi0319 <losegroove31@gmail.com>
2024-01-27 22:32:57 +0900

    1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
     images:{
         domains: ["images.microcms-assets.io", "res.cloudinary.com"],
       },
+      async headers() {
+        return [
+          {
+            source: "/api/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Origin", value: "*", },
+              { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+              { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+            ],
+          },
+        ];
+      },
     
 }
 
